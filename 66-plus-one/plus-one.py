df9ebb8ec6bd5bb2131229580eb262488959024e
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-05-14 20:28:38 +0200

    Time: 35 ms (69.58%) | Memory: 16.5 MB (84.55%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,9 +1,8 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        n = len(digits)
         jj = 0
-        for i in range(n):
-            jj += (digits[i]*10**(n-i-1))
+        for i in range(len(digits)):
+            jj += (digits[i]*10**(len(digits)-i-1))
         jj += 1
         return [int(i) for i in str(jj)]
             
