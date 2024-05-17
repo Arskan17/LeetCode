eec4418f4faefbbca1aac47a9b16d18f7cc186c7
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-05-17 23:06:31 +0200

    Time: 111 ms (5.52%) | Memory: 36.9 MB (5.09%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,14 +2,12 @@ import numpy as np
 class Solution:
     def climbStairs(self, n: int) -> int:
         staires = np.zeros(n+1)
-        staires[0] = 1
-        staires[1] = 2
         def jello(n:int, staires):
             if (n>2):
-                if (staires[n-1] != 0): return staires[n-1]
-                staires[n-2] = jello(n-1, staires)
-                staires[n-3] = jello(n-2, staires)
-                staires[n-1] = (staires[n-2] + staires[n-3])
-                return staires[n-1]
-            return staires[n-1]
+                if (staires[n] != 0): return staires[n]
+                staires[n-1] = jello(n-1, staires)
+                staires[n-2] = jello(n-2, staires)
+                staires[n] = (staires[n-2] + staires[n-1])
+                return staires[n]
+            return n
         return int(jello(n, staires))
