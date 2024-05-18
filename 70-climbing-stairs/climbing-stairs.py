3622afa9b606190cdc0d23b2c4bae44ee37514ef
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-05-18 11:50:55 +0200

    Time: 37 ms (38.46%) | Memory: 16.5 MB (30.66%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,12 +1,17 @@
-import numpy as np
 class Solution:
     def climbStairs(self, n: int) -> int:
-        staires = {}
+        staires = [0]*(n+1)
 
-        def jello(n:int, staires):
-            if (n>2):
-                if n not in staires: staires[n] = (jello(n-1, staires) + jello(n-2, staires))
-                return staires[n]
-            return n
-            
-        return int(jello(n, staires))
+        '''With recursion'''
+        # def jello(n:int, staires):
+        #     if (n>2):
+        #         if n not in staires: staires[n] = (jello(n-1, staires) + jello(n-2, staires))
+        #         return staires[n]
+        #     return n
+        # return int(jello(n, staires))
+
+        '''with loop'''
+        staires[0] = staires[1] = 1
+        for i in range(2, n+1):
+            staires[i] = staires[i-1] + staires[i-2]
+        return staires[n]
