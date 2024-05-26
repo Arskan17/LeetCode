d0344bb512e2a5206aa587b0c41a94d3c5367efa
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-05-26 23:16:10 +0200

    Time: 115 ms (36.85%) | Memory: 19.4 MB (10.16%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,11 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums = sorted(nums)
-        if len(nums) == 1: return nums[0] # Base case if size of list is 1
+        # nums = sorted(nums)
+        # if len(nums) == 1: return nums[0] # Base case if size of list is 1
 
-        if nums[0] != nums[1]: return nums[0] # second case if the first elem is the single one
-        if nums[-1] != nums[-2]: return nums[-1] # third case if the last elem is the single one
+        # if nums[0] != nums[1]: return nums[0] # second case if the first elem is the single one
+        # if nums[-1] != nums[-2]: return nums[-1] # third case if the last elem is the single one
 
-        for i in range(1, len(nums)-1):
-            if nums[i] != nums[i-1] and nums[i] != nums[i+1]:
-                return nums[i]
+        # for i in range(1, len(nums)-1):
+        #     if nums[i] != nums[i-1] and nums[i] != nums[i+1]:
+        #         return nums[i]
+        return 2*sum(set(nums)) - sum(nums)
