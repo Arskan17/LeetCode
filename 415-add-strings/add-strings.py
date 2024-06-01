9e4c8d4971be236284e5c27d1193dd8ec773cb50
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-06-01 23:54:26 +0200

    Time: 36 ms (86.61%) | Memory: 16.6 MB (95.12%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -1,4 +1,11 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        sys.set_int_max_str_digits(50000)
+        sys.set_int_max_str_digits(10000)
         return str(int(num1) + int(num2))
+
+        # n = int(num1) + int(num2)
+        # jj = ""
+        # while n:
+        #     jj+= str(n%10)
+        #     n = n//10
+        # return jj
