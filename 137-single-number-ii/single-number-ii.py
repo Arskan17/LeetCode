613f41ed8487e7d506823220c195e947e58c3361
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-06-05 23:38:13 +0200

    Time: 54 ms (75.74%) | Memory: 18.7 MB (30.02%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -1,15 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         jj = {}
+        # Iterate through the list and add keep count of the number of times each element appears, by using a dictionary
         for n in nums:
             if n in jj:
                 jj[n] += 1
                 if jj[n] == 3:
                     del jj[n]
-            else:
-                jj[n] = 1
+            else: jj[n] = 1
 
         # loop through the dictionary untill we find a key who's value is 1, return that key.        
-        # for k, v in jj.items():
-        #     if v == 1: return k
-        return next(iter(jj), None)
+        for k, v in jj.items():
+            if v == 1: return k
+        # return next(iter(jj), None)
