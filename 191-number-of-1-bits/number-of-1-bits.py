9e67b498d3fa6bdd3fa6161879c867b43ec4d3d7
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-06-08 13:39:29 +0200

    Time: 37 ms (45.74%) | Memory: 16.4 MB (82.86%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,11 +1,8 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         i = 0
-        jj = format(n, 'b') # format() converts its first param depending on its second, in this case 'b' for binary
-
+        jj = format(n, 'b')
         for j in jj:
-            if int(j) == 1:
-                i += 1
-
+            if int(j) == 1: i+=1
         return i
         
