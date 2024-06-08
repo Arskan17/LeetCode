bdb8bec12ae9b8b23d4bff9dbde1c1ce2fcaf4a8
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-06-08 15:30:58 +0200

    Time: 60 ms (5.18%) | Memory: 17.5 MB (5.66%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -3,30 +3,18 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        # for i in range(len(s)):
-        #     s_opposite = t[i]
-        #     for j in range(i+1, len(t)):
-        #         if (s[j] == s[i]) and (s_opposite != t[j]):
-        #             return False
-        #         if (s[j] != s[i]) and (s_opposite == t[j]):
-        #             return False
-
-        # return True
-
-
 
         record_of_ss_letters = {}
         record_of_ts_letters = {}
 
-        for index, letter in enumerate(s):
-            if letter not in record_of_ss_letters:
-                record_of_ss_letters[letter] = ""
-            record_of_ss_letters[letter] += str(index)
+        for index, (letter_s, letter_t) in enumerate(zip(s, t)):
+            if letter_s not in record_of_ss_letters:
+                record_of_ss_letters[letter_s] = ""
+            if letter_t not in record_of_ts_letters:
+                record_of_ts_letters[letter_t] = ""
 
-        for index, letter in enumerate(t):
-            if letter not in record_of_ts_letters:
-                record_of_ts_letters[letter] = ""
-            record_of_ts_letters[letter] += str(index)
+            record_of_ss_letters[letter_s] += str(index)
+            record_of_ts_letters[letter_t] += str(index)
 
         for indexes in record_of_ss_letters.values():
             if indexes not in record_of_ts_letters.values():
@@ -34,8 +22,3 @@ class Solution:
 
 
         return True
-
-
-        # return len(record_of_ss_letters) == len(record_of_ts_letters)
-
-        # return len(set(s)) == len(set(t))
