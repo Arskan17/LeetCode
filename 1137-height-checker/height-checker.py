d42139f02e2f7e2d92e0aee4f77d64d6d412b9b0
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-06-10 22:37:31 +0200

    Time: 34 ms (85.97%) | Memory: 16.3 MB (96.99%) - LeetSync

--- 1137-height-checker/height-checker.py
+++ 1137-height-checker/height-checker.py
@@ -1,10 +1,13 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         expected = sorted(heights)
-        wrongHeight = 0
-        for i, height in enumerate(heights):
-            if height != expected[i]:
-                wrongHeight += 1
+        # wrongHeight = 0
+
+        # for i, height in enumerate(heights):
+        #     if height != expected[i]:
+        #         wrongHeight += 1
         
-        return wrongHeight
+        # return wrongHeight
         
+
+        return len([1 for i, height in enumerate(heights) if height != expected[i]])
