881e62d15d568e68c2a6e511a337446f832d036e
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-06-13 15:01:30 +0200

    Time: 564 ms (5.03%) | Memory: 34.3 MB (11.40%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,8 +1,27 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        for i, num in enumerate(nums):
-            if num in nums[i+1:i+k+1]:
-                return True
+        # for i, num in enumerate(nums):
+        #     if num in nums[i+1:i+k+1]:
+        #         return True
 
+        # return False
+        if len(nums)<2:
+            return False
+
+        position_records= {}
+        for index, num in enumerate(nums):
+            if num not in position_records:
+                position_records[num] = []
+            tempRecord = position_records[num]
+            tempRecord.append(index)
+            position_records[num] = tempRecord
+
+        positions = [position for position in position_records.values()] # get back those indexes from the dictionart into a list
+
+        distances = [(indexes[i+1]-indexes[i]) for indexes in positions for i in range(len(indexes)-1)] # if (indexes[i+1]-indexes[i]) <= k )
+        for distance in distances:
+            if distance <= k:
+                return True
+        
         return False
         
