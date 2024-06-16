9517d6c6a64661ca573c0ec1c9b8af09ae9923c5
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-06-16 22:23:41 +0200

    Time: 52 ms (32.64%) | Memory: 16.7 MB (31.17%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -2,4 +2,4 @@ class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         setOfNum1 = set(nums1)
         setOfNum2 = set(nums2)
-        return set([commonNum for commonNum in setOfNum1 if commonNum in setOfNum2])
+        return [commonNum for commonNum in setOfNum1 if commonNum in setOfNum2]
