d5074a175d3daec332331b211c9c75e336ac6fd7
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-06-16 21:21:30 +0200

    Time: 169 ms (52.41%) | Memory: 20.8 MB (78.68%) - LeetSync

---
+++ 344-reverse-string/reverse-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        inversedString = [s[index] for index in range(len(s)-1, -1, -1)]
+
+        for index, letter in enumerate(inversedString):
+            s[index] = letter
+        
