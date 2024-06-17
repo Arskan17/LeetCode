b50ef952b4ce4ac100a5b21153ca089ca934f8dc
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-06-17 23:10:42 +0200

    Time: 48 ms (7.33%) | Memory: 16.4 MB (78.45%) - LeetSync

--- 258-add-digits/add-digits.py
+++ 258-add-digits/add-digits.py
@@ -3,7 +3,7 @@ class Solution:
         if num < 10:
             return num
 
-        jj = [int(digit) for digit in str(num)]
-        num = sum(jj)
+        # jj = [int(digit) for digit in str(num)]
+        # num = sum(jj)
         
-        return Solution().addDigits(num)
+        return Solution().addDigits(sum([int(digit) for digit in str(num)]))
