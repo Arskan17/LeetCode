0a2d1e49681feeec905cff84e82f30890e4c7f36
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2024-07-17 13:34:56 +0200

    Time: 61 ms (57.38%) | Memory: 17.7 MB (25.41%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,4 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        return (next((i, j) for i in range(n) for j in range(i+1, n) if nums[i] + nums[j] == target))
+        setRecord = {} # initalaize dictionary
+
+        for index, elem in enumerate(nums):
+            complement = target - elem # generating the other number
+            if complement in setRecord: # if it's alreaddy in the dictionary, we can just give back the index
+                return [setRecord[complement], index]
+            else:
+                setRecord[elem] = index # if it's not yet in the dictionary, put it in
+        return
