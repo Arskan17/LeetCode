3055f3c0a2fba7246940cb93cef1f3cd60005411
Arsène <75216911+Arskan17@users.noreply.github.com>
2024-07-29 21:36:11 +0200

    Time: 27 ms (94.81%) | Memory: 16.4 MB (49.17%) - LeetSync

--- 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.py
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.py
@@ -7,8 +7,15 @@ class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
         binNumber = ""
 
+        # get all the digits from the linked-list into a string
         while head:
             binNumber += str(head.val)
             head = head.next
 
-        return int(binNumber, 2)
+        # then traverse the list in reverse order and multiply every digit by two to the power of its position in the string
+        # it's the same as doing `int(binNumber, 2)`, but uses less memory
+        decNumber = 0
+        for i, digit in enumerate(binNumber[::-1]):
+            decNumber += int(digit) * (2**i)
+
+        return decNumber
