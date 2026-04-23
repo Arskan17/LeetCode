8b3321ef112938da79d650e300de94dd3a58f62e
Arsène <75216911+Arskan17@users.noreply.github.com>
2026-04-23 09:48:24 +0200

    Time: 750 ms (50.07%) | Memory: 21.9 MB (17.65%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.py
@@ -0,0 +1,13 @@
+class Solution:
+    
+    def concatenatedBinary(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return n
+            
+        binary_string = ""
+
+        for i in range(1, n+1):
+            binary_string += bin(i)[2:]
+
+        return int(binary_string, 2) % ((10**9) + 7)
+        
