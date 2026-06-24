bdfa8bf685aa362deb4acfc7639dd352d6b5365c
Arsène <75216911+Arskan17@users.noreply.github.com>
2026-06-24 12:10:29 +0200

    Time: 62 ms (11.08%) | Memory: 19.4 MB (54.38%) - LeetSync

---
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.py
@@ -0,0 +1,14 @@
+class Solution:
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        ls = [0] # put 0 in front
+        # given that the last elem in `ls` is always the current sum of the list,
+        # just take that, and sum with the current elem in `nums`.
+        for n in nums[:-1]:
+            ls.append(ls[-1]+n)
+
+
+        # summ the right slice for every index, untill the second to last
+        rs = [sum(nums[i+1:]) for i in range(len(nums)-1)]
+        rs.append(0) # append the 0 to fill
+
+        return [abs(l-r) for r, l in zip(ls,rs)]
